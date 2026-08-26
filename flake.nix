a7369791f3df53acb2bbd38720f336e05b6c74b4
Juhnowski <juhnowski@gmail.com>
2026-08-26 18:25:01 +0300

    first commit

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 # /home/ilya/TestVerilog/flake.nix
 {
-  description = "OCSSD Development Environment with verilog-pcie tests";
+  description = "OCSSD Dev Environment: Clean Python & Verilog Simulation";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -11,38 +11,44 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        
-        # Исправленная функция сборки расширений
-buildCocotbExt = pname: version: hash: deps: pkgs.python311.pkgs.buildPythonPackage {
-  inherit pname version;
-  src = pkgs.python311.pkgs.fetchPypi { 
-    inherit pname version hash; 
-  };
-  # ИСПОЛЬЗУЙТЕ ТОЧКИ: pkgs . python311Packages . cocotb
-  propagatedBuildInputs = [ pkgspythonPackagescocotb ] ++ deps;
-  doCheck = false;
-};
 
+        customCocotb = pkgs . python311Packages . buildPythonPackage rec {
+          pname = "cocotb";
+          version = "2.0.1";
+          src = pkgs . fetchFromGitHub {
+            owner = "cocotb";
+            repo = "cocotb";
+            rev = "v${version}";
+            hash = "sha256-LXQNqFlvP+WBaDGWPs5+BXBtW2dhDu+v+7lR/AMG21M="; # Хеш зафиксирован
+          };
+          pyproject = true;
+          
+          nativeBuildInputs = with pkgs . python311Packages; [ setuptools wheel ];
+          
+          # ФИКС: Прокидываем find-libpython как runtime-зависимость для cocotb
+          propagatedBuildInputs = with pkgs . python311Packages; [ find-libpython ];
+          
+          doCheck = false;
+        };
 
-
-        pythonEnv = pkgs.python311.withPackages (ps: with ps; [
-          cocotb
+        pythonEnv = pkgs . python311 . withPackages (ps: with ps; [
+          customCocotb
           pytest
-          # Пример добавления расширений с заглушками хешей
-          (buildCocotbExt "cocotbext-axi" "0.2.2" "sha-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=" [])
-          (buildCocotbExt "cocotbext-pcie" "0.2.1" "sha256-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=" [ 
-            (buildCocotbExt "cocotbext-common" "0.1.2" "sha-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC=" [])
-          ])
         ]);
       in {
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs . mkShell {
           buildInputs = with pkgs; [
             iverilog
             gtkwave
             pythonEnv
-            git
             gnumake
+            git
           ];
+
+          shellHook = ''
+            echo "⚡ [Enterprise Lab] Окружение полностью готово."
+            echo "Icarus Verilog + Cocotb успешно установлены!"
+          '';
         };
       });
 }
